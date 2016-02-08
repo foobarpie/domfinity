184f5a8d3e02d9fccfe3d17ddd1f0a0ad6fe7e71
Foo Bar <foobarpie.gmail.com>
2016-02-07 19:01:17 -0500

    first commit

---
+++ js/polyfill.js
@@ -0,0 +1,22 @@
+/**
+ * Created by timur on 2/7/16.
+ */
+// This makes it so changing the textContent will work on ie8
+if (Object.defineProperty
+  && Object.getOwnPropertyDescriptor
+  && Object.getOwnPropertyDescriptor(Element.prototype, "textContent")
+  && !Object.getOwnPropertyDescriptor(Element.prototype, "textContent").get) {
+  (function() {
+    var innerText = Object.getOwnPropertyDescriptor(Element.prototype, "innerText");
+    Object.defineProperty(Element.prototype, "textContent",
+      {
+        get: function() {
+          return innerText.get.call(this);
+        },
+        set: function(s) {
+          return innerText.set.call(this, s);
+        }
+      }
+    );
+  })();
+}
